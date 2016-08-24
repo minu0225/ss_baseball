9e8b0d6fe4994361e4621a5fab959fad67413579
minu0225 <16space14@sdh.hs.kr>
2016-08-24 21:31:31 +0900

    second_project

---
+++ baseball/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>야구게임</title>
+    <link rel="stylesheet" href="./baseball.css">
+</head>
+<body>
+  <form action="baseball.php" method="post">
+   <header>
+   <div id="title">야구게임</div>
+   </header>
+   
+   <section>
+      <article>
+      <div id="com">
+       <div class="com1"></div>
+       <div class="com2"></div>
+       <div class="com3"></div>
+          </div>
+          <div id="result"></div>
+          
+          <div id="user">
+        <input type="text" name="user1" class="user1" placeholder="0">
+        <input type="text" name="user2" class="user2" placeholder="0">
+        <input type="text" name="user3" class="user3" placeholder="0">
+          </div>
+          <input type="button" value="재시작">
+          <input type="submit" value="">
+   </article>
+   </section>
+   
+   <footer>
+       
+   </footer>
+   </form>
+</body>
+</html>
