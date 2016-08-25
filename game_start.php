6f4d1d385246390c1e993e78e0c026b1f1030e79
minu0225 <16space14@sdh.hs.kr>
2016-08-25 17:01:06 +0900

    second_project

---
+++ game_start.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+$strike = 0;
+if($_POST['user1'] = $_SESSION['com_1']){
+    $strike = +1;
+}
+include "index.php";
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,33 +9,52 @@
     <link rel="stylesheet" href="./baseball.css">
 </head>
 <body>
-  <form action="baseball.php" method="post">
+  <form action="game_start.php" method="post">
    <header>
    <div id="title">야구게임</div>
    </header>
-   
    <section>
       <article>
+      <?php
+          $com1 = mt_rand(1,9);
+          $com2 = mt_rand(1,9);
+          $com3 = mt_rand(1,9);
+          $_SESSTION['com_1'] = $com1;
+          $_SESSTION['com_2'] = $com2;
+          $_SESSTION['com_3'] = $com3;
+          echo"<script>console.log('$com1 $com2 $com3')</script>"
+          ?>
       <div id="com">
-       <div class="com1"></div>
-       <div class="com2"></div>
-       <div class="com3"></div>
+       <div class="com1">?</div>
+       <div class="com2">?</div>
+       <div class="com3">?</div>
           </div>
-          <div id="result"></div>
+          <div id="result">
+             <?php
+              echo "$strike";
+              ?>
+              </div>
           
           <div id="user">
         <input type="text" name="user1" class="user1" placeholder="0">
         <input type="text" name="user2" class="user2" placeholder="0">
         <input type="text" name="user3" class="user3" placeholder="0">
+        <div id="button">
+         <input type="button" value="재시작" class="reset" onclick="reset()">
+          <input type="submit" value="시작" class="start">
+              </div>
           </div>
-          <input type="button" value="재시작">
-          <input type="submit" value="">
+          <script>
+          function reset(){
+              
+          }
+          </script>
    </article>
    </section>
    
    <footer>
        
    </footer>
-   </form>
+    </form>
 </body>
 </html>
